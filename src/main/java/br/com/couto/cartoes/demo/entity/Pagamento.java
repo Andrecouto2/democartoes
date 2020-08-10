763913663b248889fe5cf79f6008847eaec7cb7f
andrecouto <coutoandreluis@gmail.com>
2020-08-10 17:29:01 -0300

    adjust entities

--- src/main/java/br/com/couto/cartoes/demo/entity/Pagamento.java
+++ src/main/java/br/com/couto/cartoes/demo/entity/Pagamento.java
@@ -1,36 +1,40 @@
 package br.com.couto.cartoes.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "pagamento")
 public class Pagamento {
 
     @Id
     @GeneratedValue
     @Getter
     @Setter
+    @Column(name = "id", nullable = false, unique = true)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long id;
 
     @Setter
     @Getter
+    @Column
     private String descricao;
 
     @Setter
     @Getter
+    @Column
     private Double valor;
 
-    @OneToOne
     @Getter
     @Setter
+    @OneToOne
+    @JoinColumn(name = "cartao_id", referencedColumnName = "id")
     private Cartao cartao;
 }
